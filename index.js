b17a98413fa406c4ccd459842ddb804950b446e6
IgoryanTV0 <avatar200297@gmail.com>
2022-11-26 16:12:46 +0300

    Использование шаблонизатора ON

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const path = require('path');
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: 'remotemysql.com',
@@ -8,6 +9,7 @@ const pool = mariadb.createPool({
 });
 
 var app = express();
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.ejs`)
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +19,13 @@ app.use('/css', express.static('web/css'));
 app.use('/js', express.static('web/js'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/web/index.html");
+    const title = 'Главная';
+    res.render(createPath('index'), { title });
+});
+
+app.get('/diagram', function(req, res){
+    const title = 'Редактор диаграмм';
+    res.render(createPath('diagram'), { title });
 });
 
 app.use((req, res) => {
